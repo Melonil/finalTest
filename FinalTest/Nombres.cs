85a699d350eef80c162b2740d13032770cf3edf4
p1413976 <yaci.rachidi@gmail.com>
2015-06-25 10:12:53 +0200

    recuperer les nombres pairs

---
+++ FinalTest/Nombres.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest
+{
+    public class Nombres
+    {
+        private readonly IEnumerable<KeyValuePair<string, int>> nombresPairs;
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            this.nombresPairs = keyValuePairs;
+        }
+
+        public IEnumerable<int> NombresPairs
+        {
+            get
+            {
+                return nombresPairs.Where(x => (x.Value % 2) == 0).Select(x => x.Value);
+            }
+        }
+
+    }
+}

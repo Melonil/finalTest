ce62fd6b2ced9ef440f03fd56aec7b2176af08c9
p1413976 <yaci.rachidi@gmail.com>
2015-06-25 09:12:09 +0200

    comparer deux réfenrence

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest.Tests
+{
+    public class TypeReference
+    {
+        private int valeur;
+        public TypeReference(int i)
+        {
+            this.valeur = i;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false; if (obj.GetType() != this.GetType()) return false; TypeReference other = (TypeReference)obj; return other.valeur == this.valeur;
+        }
+
+    }
+
+
+}
